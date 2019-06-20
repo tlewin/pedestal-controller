468ca4373102a624cb5e41f05718e565ee4eff59
Thiago Lewin <thiago.lewin@gmail.com>
2019-06-20 22:03:07 +0200

    Initial implementation of defcontroller
    The main idea behind controllers is to define a set of
    handlers that shares the same interceptors/ settings,
    making the entire process less error-prone and easy to maintain.
    
    ```
    (defcontroller Product
      (interceptors [default
                     auth])
    
      (handler create
               (fn [request]
                 (let [{{data :data} :params} request]
                   (ring-resp/response (build-product! data)))))
    
      (handler search
               (fn [request]
                 (let [{{query :query} :params} request]
                   (ring-resp/response (render (search query)))))))
    ```
    
    Then, after:
    
    ```
    (interceptors Product)    ;; [default auth]
    (handlers Product)        ;; {:create (fn ...) :search (fn ...)}
    (handler Product :create) ;; (fn ... )
    ```
    
    Some tests included, but still missing some.

--- src/pedestal_controller/core.clj
+++ src/pedestal_controller/core.clj
@@ -1,6 +1,75 @@
 (ns pedestal-controller.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defprotocol IController
+  (interceptors [this])
+  (handlers [this])
+  (handler [this handler-name]))
+
+(defrecord Controller [interceptors
+                       handlers]
+  IController
+  (interceptors [this] interceptors)
+  (handlers [this] handlers)
+  (handler
+    [this handler-name]
+    (get handlers handler-name)))
+
+(defn- collify
+  [v]
+  (cond
+    (nil? v) []
+    (coll? v) v
+    :else [v]))
+
+(defn- fail!
+  [message]
+  (throw (Exception. message)))
+
+(defn- validate-interceptors-params!
+  [params]
+  (when (not= (count params) 1)
+    (fail! "Invalid arguments for interceptors: (interceptors [...]).")))
+
+(defn- validate-handler-params!
+  [params]
+  (when (not= (count params) 2)
+    (fail! "Invalid arguments for handler: (handler handler-name handler-fn).")))
+
+(defn- reduce-controller-settings
+  [settings]
+  (reduce
+   (fn [[interceptors handlers] [item & params]]
+     (case item
+       interceptors
+       (if (empty? interceptors)
+         (do
+           (validate-interceptors-params! params)
+           [(collify (first params)) handlers])
+         (fail! "interceptors is defined more than once."))
+
+       handler
+       (let [[handler-name handler-fn] params
+             handler-name              (keyword handler-name)]
+         (if (contains? handlers handler-name)
+           (fail! (str handler-name " is already defined."))
+           (do
+             (validate-handler-params! params)
+             [interceptors (assoc handlers handler-name handler-fn)])))
+
+       ;; Anything else
+       (fail! (str item " is an unknown setting."))))
+   ;; [Interceptors Handlers]
+   [[] {}]
+   settings))
+
+(defmacro defcontroller
+  [controller-name & settings]
+  `(def ~controller-name
+     (Controller. ~@(reduce-controller-settings (map collify settings)))))
+
+(defn controller?
+  [o]
+  (instance? Controller o))
+
+(defn remap-routes
+  [routes])
--- test/pedestal_controller/core_test.clj
+++ test/pedestal_controller/core_test.clj
@@ -2,6 +2,64 @@
   (:require [clojure.test :refer :all]
             [pedestal-controller.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-defcontroller
+  (testing "validates already defined interceptor"
+    (is (thrown-with-msg?
+         Exception
+         #"interceptors is defined more than once."
+         (binding [*ns* (find-ns 'pedestal-controller.core)]
+           (eval '(defcontroller Invalid
+                    (interceptors [:a])
+                    (interceptors [:b])))))))
+  (testing "validates missing argument for interceptors"
+    (is (thrown-with-msg?
+         Exception
+         #"Invalid arguments for interceptors"
+         (binding [*ns* (find-ns 'pedestal-controller.core)]
+           (eval '(defcontroller Invalid
+                    (interceptors)))))))
+  (testing "validates trailing argument for interceptors"
+    (is (thrown-with-msg?
+         Exception
+         #"Invalid arguments for interceptors"
+         (binding [*ns* (find-ns 'pedestal-controller.core)]
+           (eval '(defcontroller Invalid
+                    (interceptors :a :b)))))))
+  (testing "validates already defined handler"
+    (is (thrown-with-msg?
+         Exception
+         #":h1 is already defined."
+         (binding [*ns* (find-ns 'pedestal-controller.core)]
+           (eval '(defcontroller Invalid
+                    (handler :h1 identity)
+                    (handler :h1 identity)))))))
+  (testing "validates missing argument for handler - 1"
+    (is (thrown-with-msg?
+         Exception
+         #"Invalid arguments for handler"
+         (binding [*ns* (find-ns 'pedestal-controller.core)]
+           (eval '(defcontroller Invalid
+                    (handler)))))))
+  (testing "validates missing argument for handler - 2"
+    (is (thrown-with-msg?
+         Exception
+         #"Invalid arguments for handler"
+         (binding [*ns* (find-ns 'pedestal-controller.core)]
+           (eval '(defcontroller Invalid
+                    (handler :h1)))))))
+  (testing "validates trailing argument for handler"
+    (is (thrown-with-msg?
+         Exception
+         #"Invalid arguments for handler"
+         (binding [*ns* (find-ns 'pedestal-controller.core)]
+           (eval '(defcontroller Invalid
+                    (handler :h1 identity :extra)))))))
+  (testing "validates invalid controller setting"
+    (is (thrown-with-msg?
+         Exception
+         #"typo is an unknown setting."
+         (binding [*ns* (find-ns 'pedestal-controller.core)]
+           (eval '(defcontroller Invalid
+                    (interceptors [:a])
+                    (handler :h1 identity)
+                    (typo))))))))
