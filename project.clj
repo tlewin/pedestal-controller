ee2f60704a4a7a9c29e1fc25dd8f2f63c6165ac2
Thiago Lewin <thiago.lewin@gmail.com>
2019-06-20 22:45:18 +0200

    Update project.clj description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject pedestal-controller "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Simple controller mechanism for Pedestal applications"
+  :url "https://github.com/tlewin/pedestal-controller"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]])
