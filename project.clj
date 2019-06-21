adf0be12bbc804cafc550b08389a0f68628cac93
Thiago Lewin <thiago.lewin@gmail.com>
2019-06-21 23:43:15 +0200

    Test against multiple Clojure versions

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "https://github.com/tlewin/pedestal-controller"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :aliases {"test-all" ["with-profile" "base:+1.9:+1.10" "test"]}
+  :profiles {:dev  {:dependencies [[org.clojure/clojure "1.10.0"]]}
+             :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
+             :1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}})
