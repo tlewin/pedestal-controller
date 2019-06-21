dc660a2e4747c8bb7b76d89f27e6a48a67b82481
Thiago Lewin <thiago.lewin@gmail.com>
2019-06-21 23:19:06 +0200

    Workaround for testing macro's CompilerException
    Clojure 1.10 wraps the exceptions raised during the
    macro expansion phase into CompileriException class,
    breaking some tests that relies on `thrown-with-msg?`.
    
    This diffs tests both cases, making the tests
    backward compatible to older Clojure versions.

--- test/pedestal_controller/core_test.clj
+++ test/pedestal_controller/core_test.clj
@@ -1,10 +1,11 @@
 (ns pedestal-controller.core-test
   (:require [clojure.test :refer :all]
+            [pedestal-controller.test-utils :refer :all]
             [pedestal-controller.core :refer :all]))
 
 (deftest test-defcontroller
   (testing "validates already defined interceptor"
-    (is (thrown-with-msg?
+    (is (macro-thrown-with-msg?
          Exception
          #"interceptors is defined more than once."
          (binding [*ns* (find-ns 'pedestal-controller.core)]
@@ -12,21 +13,21 @@
                     (interceptors [:a])
                     (interceptors [:b])))))))
   (testing "validates missing argument for interceptors"
-    (is (thrown-with-msg?
+    (is (macro-thrown-with-msg?
          Exception
          #"Invalid arguments for interceptors"
          (binding [*ns* (find-ns 'pedestal-controller.core)]
            (eval '(defcontroller Invalid
                     (interceptors)))))))
   (testing "validates trailing argument for interceptors"
-    (is (thrown-with-msg?
+    (is (macro-thrown-with-msg?
          Exception
          #"Invalid arguments for interceptors"
          (binding [*ns* (find-ns 'pedestal-controller.core)]
            (eval '(defcontroller Invalid
                     (interceptors :a :b)))))))
   (testing "validates already defined handler"
-    (is (thrown-with-msg?
+    (is (macro-thrown-with-msg?
          Exception
          #":h1 is already defined."
          (binding [*ns* (find-ns 'pedestal-controller.core)]
@@ -34,28 +35,28 @@
                     (handler :h1 identity)
                     (handler :h1 identity)))))))
   (testing "validates missing argument for handler - 1"
-    (is (thrown-with-msg?
+    (is (macro-thrown-with-msg?
          Exception
          #"Invalid arguments for handler"
          (binding [*ns* (find-ns 'pedestal-controller.core)]
            (eval '(defcontroller Invalid
                     (handler)))))))
   (testing "validates missing argument for handler - 2"
-    (is (thrown-with-msg?
+    (is (macro-thrown-with-msg?
          Exception
          #"Invalid arguments for handler"
          (binding [*ns* (find-ns 'pedestal-controller.core)]
            (eval '(defcontroller Invalid
                     (handler :h1)))))))
   (testing "validates trailing argument for handler"
-    (is (thrown-with-msg?
+    (is (macro-thrown-with-msg?
          Exception
          #"Invalid arguments for handler"
          (binding [*ns* (find-ns 'pedestal-controller.core)]
            (eval '(defcontroller Invalid
                     (handler :h1 identity :extra)))))))
   (testing "validates invalid controller setting"
-    (is (thrown-with-msg?
+    (is (macro-thrown-with-msg?
          Exception
          #"typo is an unknown setting."
          (binding [*ns* (find-ns 'pedestal-controller.core)]
---
+++ test/pedestal_controller/test_utils.clj
@@ -0,0 +1,46 @@
+(ns pedestal-controller.test-utils
+  (:require [clojure.test :refer :all])
+  (:import [clojure.lang Compiler Compiler$CompilerException]))
+
+;; Original from https://github.com/clojure/clojure/blob/master/src/clj/clojure/test.clj#L518
+;; It's was changed to be compatible with Clojure 1.10.0 where the any exception
+;; raised during the macro expansion, it's wrapped in the Compiler$CompilerException
+(defmethod assert-expr 'macro-thrown-with-msg? [msg form]
+  ;; (is (thrown-with-msg? c re expr))
+  ;; Asserts that evaluating expr throws an exception of class c.
+  ;; Also asserts that the message string of the exception matches
+  ;; (with re-find) the regular expression re.
+  (let [klass (nth form 1)
+        re    (nth form 2)
+        body  (nthnext form 3)]
+    `(try ~@body
+          (do-report {:type     :fail
+                      :message  ~msg
+                      :expected '~form
+                      :actual   nil})
+          (catch Compiler$CompilerException ce#
+            (let [cause# (.getCause ce#)
+                  m#     (.. ce# getCause getMessage)]
+              (if (and (instance? ~klass cause#)
+                       (re-find ~re m#))
+                (do-report {:type     :pass
+                            :message  ~msg
+                            :expected '~form
+                            :actual   ce#})
+                (do-report {:type     :fail
+                            :message  ~msg
+                            :expected '~form
+                            :actual   ce#})))
+            ce#)
+          (catch ~klass e#
+            (let [m# (.getMessage e#)]
+              (if (re-find ~re m#)
+                (do-report {:type     :pass
+                            :message  ~msg
+                            :expected '~form
+                            :actual   e#})
+                (do-report {:type     :fail
+                            :message  ~msg
+                            :expected '~form
+                            :actual   e#})))
+            e#))))
