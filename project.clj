0897063ae1c0373417f687722fd7506ae33834d7
Thiago Lewin <thiago.lewin@gmail.com>
2019-06-24 23:22:40 +0200

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pedestal-controller "0.1.0"
+(defproject pedestal-controller "0.1.1"
   :description "Simple controller mechanism for Pedestal applications"
   :url "https://github.com/tlewin/pedestal-controller"
   :license {:name "Eclipse Public License"
